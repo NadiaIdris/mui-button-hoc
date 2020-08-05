c4379ddbf1f848ddd6a0f79fb9f87590b4ee3618
Maret Idris <maret@fasterlap.com>
2020-08-04 18:42:58 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {UglyButton, PrettyButton} from "./MyButton";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <UglyButton text="MUI button with HOC" />
+                <PrettyButton text="Today" />
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
